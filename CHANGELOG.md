cd358fcc7308406483ce7c6e21004ad40c46c70e
mg3994 <manishgautammg7@yahoo.com>
2024-07-17 21:54:37 +0530

    chore

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,11 @@
+## 0.0.2-dev.1
+
+ - improved code
+ - abstract and sealed class detection
+ - better error reporting
+  
 ## 0.0.1-dev.1
 
 - Initial version.
 - @Singleton added
   
-## 0.0.1-dev.2
- - improved code
- - abstract and sealed class detection
- - better error reporting
--- bump_version.sh
+++ bump_version.sh
@@ -7,17 +7,26 @@ increment_version() {
   local version=$1
   local delimiter=.
   local array=($(echo "$version" | tr $delimiter '\n'))
+
   if [[ $version == *"-dev"* ]]; then
-    array[2]=$((array[2] + 1))
-    echo "${array[0]}.${array[1]}.${array[2]}-dev.${array[3]:-1}"
+    # Increment the dev version part
+    local dev_version=${array[2]}
+    array[2]=$((dev_version + 1))
+    new_version="${array[0]}.${array[1]}.${array[2]}-dev.${array[3]:-1}"
   else
-    array[2]=$((array[2] + 1))
-    echo "${array[0]}.${array[1]}.${array[2]}"
+    # Increment the stable version part
+    local stable_version=${array[2]}
+    array[2]=$((stable_version + 1))
+    new_version="${array[0]}.${array[1]}.${array[2]}"
   fi
+  
+  echo "$new_version"
 }
 
 # Read the current version from pubspec.yaml
 current_version=$(grep '^version: ' pubspec.yaml | sed 's/version: //')
+
+# Increment the version
 new_version=$(increment_version $current_version)
 
 # Update the version in pubspec.yaml
